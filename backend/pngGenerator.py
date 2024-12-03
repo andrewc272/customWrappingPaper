987d9148974a469fd66c33be7ad4020cbd59a63a
Andrew Carlson <andrewcarlson272@gmail.com>
2024-12-03 11:33:49 -0600

    save 12.2.24

--- backend/pngGenerator.py
+++ backend/pngGenerator.py
@@ -25,7 +25,9 @@ image_left = Image.open("Images/left.png")
 image_up = Image.open("Images/up.png")
 image_down = Image.open("Images/down.png")
 
-canvas = Image.new('RGB', (0,0), RGB_white)
+canvas_height = 0
+canvas_width = 0
+canvas = Image.new('RGB', (canvas_width, canvas_height), RGB_white)
 canvas_grid = []
 
 # box
@@ -34,12 +36,12 @@ box_dimensions = (-1, -1, -1) # height, width, depth
 def main():
     getDimensions()
     makeCanvas()
-    fillCanvas(0, 0)
+    fillCanvas()
     showCanvas()
 
 def getDimensions():
     for i in range(0,3):
-        box_dimensions[i] = (float)input(f'What is dimension i: ')
+        box_dimensions[i] = (float)input(f'What is dimension {i} in inches: ') * 1000
     box_dimensions.sort(reverse=True)
 
 def makeCanvas():
@@ -47,13 +49,30 @@ def makeCanvas():
     canvas_height = ( box_depth * 1.5 ) + box_height
     canvas_width = ( 2 * box_width ) + ( 2 * box_depth )
     canvas = Image.new('RGB', (canvas_width, canvas_height), RGB_white)
+    canvas_grid = ([0, box_width, box_width+box_depth, 2*box_width+box_depth, 2*box_width+2*box_depth],
+                   [0, ])
+            
 
 
-def fillCanvas(x, y):
-    if isGrid(x, y):
+def fillCanvas():
+    makeImages()
+    drawImages()
 
-def isGrid(x, y):
-    box_height, box_width, box_depth = box_dimensions
+    '''
+There are a few different ways of doing this and I want to find the one that will be simplest and fastest
+    - you can make 13 different images and then draw them on to the canvas in order
+        - up 1, up 2, up 3, up 4
+        - front
+        - left
+        - back
+        - right
+        - down 1, down 2, down 3, down 4
+        - grid
+
+    - Another option is to make the grid and then fill in each square of the grid given an image
+        - this would have to in real time map the image to the squares rather than drawing 
+            a pre-existing image.
+    '''
 
 
 
